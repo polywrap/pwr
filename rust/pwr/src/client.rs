92a68da063f0c0654d7114baf2a09c4972991bd6
Jure Bogunovic <bogunovij@gmail.com>
2023-04-27 16:50:48 +0200

    ported most of pwr-js to rust

---
+++ rust/pwr/src/client.rs
@@ -0,0 +1,117 @@
+use polywrap_client::client::*;
+use polywrap_client::core::env::Env;
+use polywrap_client::core::error::Error;
+use polywrap_client::core::loader::Loader;
+use polywrap_client::core::resolvers::uri_resolution_context::UriResolutionContext;
+use polywrap_client::core::resolvers::uri_resolver::UriResolverHandler;
+use polywrap_client::core::uri::Uri;
+use polywrap_client::core::wrapper::GetFileOptions;
+use polywrap_client_builder::{types::{ClientBuilder, BuilderConfig}, helpers::build_resolver};
+use polywrap_client_default_config::{build as build_default_config};
+
+struct SimpleError {
+    message: String
+}
+
+pub trait CoreClient {
+    fn try_resolve_uri(&self, uri: &Uri) -> Result<Uri, Error>;
+    fn invoke_raw(
+        &self,
+        uri: &Uri,
+        method: &str,
+        args: Option<&[u8]>,
+        env: Option<Env>,
+        resolution_context: Option<&mut UriResolutionContext>,
+    ) -> Result<Vec<u8>, Error>;
+    fn get_manifest(&self, uri: &Uri) -> Result<Vec<u8>, Error>;
+}
+
+pub struct CoreClientMock;
+
+impl CoreClientMock {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+impl CoreClient for CoreClientMock {
+    fn try_resolve_uri(&self, uri: &Uri) -> Result<Uri, Error> {
+        Ok(uri.clone())
+    }
+   
+    fn invoke_raw(
+        &self,
+        uri: &Uri,
+        method: &str,
+        args: Option<&[u8]>,
+        env: Option<Env>,
+        resolution_context: Option<&mut UriResolutionContext>,
+    ) -> Result<Vec<u8>, Error> {
+        Ok(vec![])
+    }
+    
+    fn get_manifest(&self, uri: &Uri) -> Result<Vec<u8>, Error> {
+        Ok(vec![])
+    }
+}
+
+pub struct PwrClient {
+    client: PolywrapClient
+}
+
+impl PwrClient {
+    pub fn new() -> Self {
+        let default_config = build_default_config();
+        let mut builder = BuilderConfig::new(Some(default_config));
+
+        let config = build_resolver(builder);
+        
+        let client = PolywrapClient::new(config);
+
+        Self {
+            client
+        }
+    }
+}
+impl CoreClient for PwrClient {
+    fn try_resolve_uri(&self, uri: &Uri) -> Result<Uri, Error> {
+        let result = self.client.try_resolve_uri(uri, None);
+
+        match result {
+            Ok(result) => Ok(result.uri()),
+            Err(e) => Err(e)
+        }
+    }
+   
+    fn invoke_raw(
+        &self,
+        uri: &Uri,
+        method: &str,
+        args: Option<&[u8]>,
+        env: Option<Env>,
+        resolution_context: Option<&mut UriResolutionContext>,
+    ) -> Result<Vec<u8>, Error> {
+        self.client.invoke(uri, method, args, env, resolution_context)
+    }
+    
+    fn get_manifest(&self, uri: &Uri) -> Result<Vec<u8>, Error> {
+        let wrapper = self.client.load_wrapper(uri, None);
+
+        match wrapper {
+            Ok(wrapper) => {
+                let wrapper = wrapper
+                    .lock()
+                    .unwrap();
+
+                let manifest = wrapper.get_file(
+                    &GetFileOptions {
+                        path: String::from("wrap.info"),
+                        encoding: None
+                    }
+                );
+
+                manifest
+            },
+            Err(e) => Err(e)
+        }
+    }
+}
--- rust/pwr/src/main.rs
+++ rust/pwr/src/main.rs
@@ -1,3 +1,92 @@
+mod app_manager;
+mod logger;
+mod client;
+mod prompter;
+
+use std::{env, fmt::Display};
+
+use polywrap_client::core::uri::Uri;
+
+use app_manager::*;
+use logger::*;
+use client::*;
+use prompter::*;
+
+fn get_pwr_dir() -> Result<String, String> {
+    let app_dir = dirs::home_dir();
+
+    if app_dir == None {
+        return Err(String::from("Error: Could not find home directory"));
+    }
+
+    let app_dir = app_dir.unwrap().join(".pwr");
+
+    // Check if the .pwr directory exists
+    if !app_dir.exists() {
+        return Err(String::from("Error: Could not find .pwr directory"));
+    }
+
+    let pwr_dir = app_dir.into_os_string().into_string().unwrap();
+
+    Ok(pwr_dir)
+}
+
+fn print_and_exit<T: Display>(error: T) {
+    println!("{}", error);
+    std::process::exit(1);
+}
+
 fn main() {
-    println!("Version 0.1.1");
+    println!("Version 0.1.2");
+
+    let _pwr_dir = get_pwr_dir().map_err(print_and_exit).unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    let args = &args[1..];
+
+    let logger = LoggerMock::new();
+    let client = PwrClient::new();
+    let manager = AppManager::new();
+    let prompter = PrompterMock::new();
+
+    let all_access_controlled_uris: Vec<String> = vec![];
+
+    let exit_code = internal_main(&args, all_access_controlled_uris, manager, &client, &logger, &prompter);
+
+    std::process::exit(exit_code);
+}
+
+pub fn internal_main(
+    args: &[String], 
+    all_access_controlled_uris: Vec<String>, 
+    manager: AppManager, 
+    client: &dyn CoreClient, 
+    logger: &dyn Logger, 
+    prompter: &dyn Prompter
+) -> i32 {
+    logger.debug(format!("Args: {:?}", args)).unwrap();
+
+    let uri = &args[0];
+
+    let uri = parse_uri(&uri);
+
+    logger.debug(format!("Parsed URI: {}", uri.to_string())).unwrap();
+
+    manager.run_app(&uri, args, all_access_controlled_uris, client, prompter, logger)
+}
+
+fn parse_uri(uri: &String) -> Uri {
+    if uri.ends_with(".eth") && !uri.starts_with("wrap://ens/") && !uri.starts_with("ens/") {
+        return Uri::try_from(format!("wrap://ens/{}", uri)).unwrap();
+    } else if uri.starts_with("Qm") {
+        return Uri::try_from(format!("wrap://ipfs/{}", uri)).unwrap();
+    } else if uri.starts_with("ipfs://") {
+        return Uri::try_from(format!("wrap://ipfs/{}", uri["ipfs://".len()..uri.len()].to_string())).unwrap();
+    } else if uri.starts_with(".") || uri.starts_with("/") {
+        return Uri::try_from(format!("wrap://file/{}", uri)).unwrap();
+    } else if !uri.contains("/") {
+        return Uri::try_from(format!("wrap://pwr/{}", uri)).unwrap();
+    } else {
+        return Uri::try_from(uri.clone()).unwrap();
+    }
 }
