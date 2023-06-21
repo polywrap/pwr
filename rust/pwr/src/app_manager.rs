0f09009304e6e27d4102a236c4b6727d2b7ef6a7
Jure Bogunovic <bogunovij@gmail.com>
2023-06-21 23:58:25 +0200

    updated code to work with newest version of the rust client

--- rust/pwr/src/app_manager.rs
+++ rust/pwr/src/app_manager.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 
 use polywrap_client::core::uri::Uri;
-use polywrap_msgpack::msgpack;
 use wrap_manifest_schemas::deserialize::{deserialize_wrap_manifest, DeserializeManifestOptions};
 
 use crate::client::CoreClient;
@@ -24,13 +23,13 @@ impl AppManager {
         &self,
         uri: &Uri, 
         args: &[String], 
-        all_access_controlled_uris: Vec<String>,
-        client: &dyn CoreClient, 
-        prompter: &dyn Prompter, 
-        logger: &dyn Logger,
+        client: &impl CoreClient, 
+        _prompter: &impl Prompter, 
+        logger: &impl Logger,
+        _all_access_controlled_uris: Vec<String>,
     ) -> i32 {
         let access_controlled_uris: Vec<String> = vec![];
-        let mut visited_uris: HashSet<String> = HashSet::new();
+        let mut _visited_uris: HashSet<String> = HashSet::new();
         
         // extract_access_controlled_uris(
         //     uri, 
@@ -41,32 +40,32 @@ impl AppManager {
         //     logger,
         // );
     
-        if access_controlled_uris.len() > 0 {
-            let response = prompter.confirm(
-                format!("App requested access to: \n{}. \nDo you want to grant access?", access_controlled_uris.join("\n"))
-            );
+        // if access_controlled_uris.len() > 0 {
+        //     let response = prompter.confirm(
+        //         format!("App requested access to: \n{}. \nDo you want to grant access?", access_controlled_uris.join("\n"))
+        //     );
 
-            let response = match response {
-                Ok(response) => response,
-                Err(promptError) => {
-                    logger.error(format!("{}", promptError));
-                    return 1;
-                }
-            };
+        //     let response = match response {
+        //         Ok(response) => response,
+        //         Err(promptError) => {
+        //             logger.error(format!("{}", promptError));
+        //             return 1;
+        //         }
+        //     };
     
-            if !response {
-                logger.error(format!("Denied access for {}", uri.to_string()));
-                return 1;
-            }
-        }
+        //     if !response {
+        //         logger.error(format!("Denied access for {}", uri.to_string()));
+        //         return 1;
+        //     }
+        // }
     
         let serialization_result = polywrap_msgpack::serialize(&AppArgs {
             args: args.to_vec(),
         });
         let args = match serialization_result {
             Ok(args) => args,
-            Err(serializeError) => {
-                logger.error(format!("{}", serializeError));
+            Err(e) => {
+                logger.error(format!("{}", e)).unwrap();
                 return 1;
             }
         };
@@ -90,74 +89,74 @@ impl AppManager {
                     Ok(exit_code) => {
                         return exit_code;
                     },
-                    Err(decodeError) => {
-                        logger.error(format!("{:?}", decodeError));
+                    Err(e) => {
+                        logger.error(format!("{:?}", e)).unwrap();
                         return 1;
                     }
                 }
             },
-            Err(invokeError) => {
-                logger.error(format!("{}", invokeError));
+            Err(e) => {
+                logger.error(format!("{}", e)).unwrap();
                 return 1;
             }
         }
     }
 }
 
-fn extract_access_controlled_uris(
-    uri: &Uri, 
-    _all_access_controlled_uris: &[String],
-    _acess_controlled_uris: &[String],
-    visited_uris: &mut HashSet<String>,
-    client: &dyn CoreClient,
-    logger: &dyn Logger,
-) {
-    if visited_uris.contains(&uri.to_string()) {
-        return;
-    }
-    visited_uris.insert(uri.to_string());
+// fn extract_access_controlled_uris(
+//     uri: &Uri, 
+//     _all_access_controlled_uris: &[String],
+//     _acess_controlled_uris: &[String],
+//     visited_uris: &mut HashSet<String>,
+//     client: &dyn CoreClient,
+//     logger: &dyn Logger,
+// ) {
+//     if visited_uris.contains(&uri.to_string()) {
+//         return;
+//     }
+//     visited_uris.insert(uri.to_string());
 
-    let serialized_manifest = client.get_manifest(uri);
-    let serialized_manifest = match serialized_manifest {
-        Ok(serialized_manifest) => serialized_manifest,
-        Err(getManifestError) => {
-            logger.error(format!("{}", getManifestError));
-            return;
-        }
-    };
+//     let serialized_manifest = client.get_manifest(uri);
+//     let serialized_manifest = match serialized_manifest {
+//         Ok(serialized_manifest) => serialized_manifest,
+//         Err(getManifestError) => {
+//             logger.error(format!("{}", getManifestError));
+//             return;
+//         }
+//     };
 
-    let manifest = deserialize_wrap_manifest(&serialized_manifest, Some(DeserializeManifestOptions {
-        no_validate: false,
-        ext_schema: None
-    }));
+//     let manifest = deserialize_wrap_manifest(&serialized_manifest, Some(DeserializeManifestOptions {
+//         no_validate: false,
+//         ext_schema: None
+//     }));
 
-    let manifest = match manifest {
-        Ok(manifest) => manifest,
-        Err(deserializeError) => {
-            logger.error(format!("{}", deserializeError));
-            return;
-        }
-    };
+//     let manifest = match manifest {
+//         Ok(manifest) => manifest,
+//         Err(deserializeError) => {
+//             logger.error(format!("{}", deserializeError));
+//             return;
+//         }
+//     };
 
-    let imported_module_types = if manifest.abi.imported_module_types != None {
-        manifest.abi.imported_module_types.unwrap() 
-    } else {
-        [].to_vec()
-    };
+//     let imported_module_types = if manifest.abi.imported_module_types != None {
+//         manifest.abi.imported_module_types.unwrap() 
+//     } else {
+//         [].to_vec()
+//     };
 
-    // TODO: figure out how to read the manifest and get the imported uris
-    // let imported_uris = imported_module_types.map(|imported_module_type| Uri::try_from(imported_module_type.uri).unwrap().to_string());
+//     // TODO: figure out how to read the manifest and get the imported uris
+//     // let imported_uris = imported_module_types.map(|imported_module_type| Uri::try_from(imported_module_type.uri).unwrap().to_string());
 
-    // let requested_uris = imported_uris.filter(|imported_uri| all_access_controlled_uris.contains(imported_uri));
-    // let other_uris = imported_uris.filter(|imported_uri| !all_access_controlled_uris.contains(imported_uri));
-    // if requested_uris.len() > 0 {
-    //     acess_controlled_uris.extend_from_slice(requested_uris);
-    // }
+//     // let requested_uris = imported_uris.filter(|imported_uri| all_access_controlled_uris.contains(imported_uri));
+//     // let other_uris = imported_uris.filter(|imported_uri| !all_access_controlled_uris.contains(imported_uri));
+//     // if requested_uris.len() > 0 {
+//     //     acess_controlled_uris.extend_from_slice(requested_uris);
+//     // }
 
-    // for other_uri in other_uris {
-    //     extract_access_controlled_uris(other_uri, client, all_access_controlled_uris, acess_controlled_uris, visited_uris);
-    // }
-}
+//     // for other_uri in other_uris {
+//     //     extract_access_controlled_uris(other_uri, client, all_access_controlled_uris, acess_controlled_uris, visited_uris);
+//     // }
+// }
 
 fn invoke_with_access_control(
     uri: &Uri,
@@ -177,8 +176,8 @@ fn invoke_with_access_control(
         Ok(data) => {
             return Ok(data);
         },
-        Err(invokeError) => {
-            return Err(format!("{}", invokeError));
+        Err(e) => {
+            return Err(format!("{}", e));
         }   
     }
 }
--- rust/pwr/src/client.rs
+++ rust/pwr/src/client.rs
@@ -1,22 +1,7 @@
-use std::fs;
 use std::sync::{Mutex, Arc};
 
-use polywrap_client::builder::helpers::build_resolver;
-use polywrap_client::builder::types::{BuilderConfig, ClientBuilder, ClientConfigHandler};
-use polywrap_client::core::client::ClientConfig;
-use polywrap_client::core::file_reader::SimpleFileReader;
-use polywrap_client::core::resolution::uri_resolution_context::UriResolutionContext;
-use polywrap_client::core::uri_resolver_handler::UriResolverHandler;
-use polywrap_client::wasm::wasm_wrapper::WasmWrapper;
-use polywrap_client::{client::*, core::wrap_loader::WrapLoader};
-use polywrap_client::core::error::Error;
-use polywrap_client::core::uri::Uri;
-use polywrap_client::core::wrapper::GetFileOptions;
-use polywrap_msgpack::msgpack;
-
-struct SimpleError {
-    message: String
-}
+use polywrap_client::{core::{resolution::uri_resolution_context::UriResolutionContext, error::Error, uri::Uri, wrap_loader::WrapLoader, wrapper::GetFileOptions, uri_resolver_handler::UriResolverHandler}, client::PolywrapClient, builder::{PolywrapClientConfig, PolywrapClientConfigBuilder}};
+use polywrap_client_default_config::{SystemClientConfig, Web3ClientConfig};
 
 pub trait CoreClient {
     fn try_resolve_uri(&self, uri: &Uri) -> Result<Uri, Error>;
@@ -59,47 +44,24 @@ impl CoreClient for CoreClientMock {
     }
 }
 
-pub struct PwrClient {
-    client: PolywrapClient
-}
+pub struct PwrClient(pub PolywrapClient);
 
 impl PwrClient {
     pub fn new() -> Self {
-        // let mut builder = BuilderConfig::new(Some(build_default_config()));
-        // builder.add_env(Uri::try_from("wrap://ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1").unwrap(), msgpack!({
-        //     "provider": "https://ipfs.wrappers.io",
-        //     "fallbackProviders": ["https://ipfs.io"],
-        //     "retries": { "tryResolveUri": 2, "getFile": 2 },
-        // }).to_vec());
-        // let config = build_resolver(builder);
-        
-        let client = PolywrapClient::new(get_config());
-        Self {
-            client
-        }
+        let mut config = PolywrapClientConfig::default();
+        config
+            .add(SystemClientConfig::default().into())
+            .add(Web3ClientConfig::default().into());
+
+        let client = PwrClient(PolywrapClient::new(config.into()));
+
+        client
     }
 }
 
-fn get_config() -> ClientConfig {
-    let manifest = fs::read("/home/nerfzael/dev/web3api/repos/pwr/packages/wrappers/echo/build/wrap.info").expect("Unable to read file");
-    let mut bytes = fs::read("/home/nerfzael/dev/web3api/repos/pwr/packages/wrappers/echo/build/wrap.wasm").expect("Unable to read file");
-
-    let config = {
-        BuilderConfig {
-            interfaces: None,
-            envs: None,
-            wrappers: Some(vec![
-            ]),
-            packages: None,
-            redirects: None,
-            resolvers: None,
-        }
-    };
-    config.build()
-}
 impl CoreClient for PwrClient {
     fn try_resolve_uri(&self, uri: &Uri) -> Result<Uri, Error> {
-        let result = self.client.try_resolve_uri(uri, None);
+        let result = self.0.try_resolve_uri(uri, None);
 
         match result {
             Ok(result) => Ok(result.uri()),
@@ -115,11 +77,11 @@ impl CoreClient for PwrClient {
         env: Option<&[u8]>,
         resolution_context: Option<Arc<Mutex<UriResolutionContext>>>,
     ) -> Result<Vec<u8>, Error> {
-        self.client.invoke(uri, method, args, env, resolution_context)
+        self.0.invoke(uri, method, args, env, resolution_context)
     }
     
     fn get_manifest(&self, uri: &Uri) -> Result<Vec<u8>, Error> {
-        let wrapper = self.client.load_wrapper(uri, None);
+        let wrapper = self.0.load_wrapper(uri, None);
 
         match wrapper {
             Ok(wrapper) => {
